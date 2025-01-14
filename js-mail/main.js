e018f76fd61197c4504058c65090bc7950a49284
413f10r <ale-fioretti@live.it>
2025-01-14 12:14:39 +0100

    start

---
+++ js-mail/main.js
@@ -0,0 +1,26 @@
+/*Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota:
+Non è necessario provvedere alla validazione delle email
+
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+Buon lavoro e buon divertimento! :slightly_smiling_face*/
+
+const MailInviti = [
+    'pippo@gmail.it',
+    'pluto@gmail.it',
+    'minnie@gmail.it',
+    'paperino@gmail.it'
+]
+
+let Mail = prompt()
